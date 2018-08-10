c71c14dd5f771ddf3dedd78e5b5d5217e22b7836
asus <asus@LAPTOP-1G900J8S>
2018-08-10 19:28:15 +0700

    Coba method length & isEmpty

--- src/Prak01.java
+++ src/Prak01.java
@@ -4,5 +4,14 @@ public class Prak01
     {
         String identitas = "Arya Putra Adyartama / X RPL 3 / 7";
         System.out.println("Identitas : " + identitas);
+        
+        String x ="Operasi";
+        System.out.println("Isi Variabel x : " + x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("Isi Variabel y : " + y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
     }
 }
