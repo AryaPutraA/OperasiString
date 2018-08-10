e38596c47233fff3bfc786c4780d28cb0b3e9408
asus <asus@LAPTOP-1G900J8S>
2018-08-10 20:05:48 +0700

    Coba method substring, replace, & split

---
+++ src/Tgs01.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Tgs01 
+{
+    public static void A(String a)
+    {
+        Scanner o = new Scanner(System.in);
+        System.out.println("");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Nama  : Arya Putra Adyartama");
+        System.out.println("Kelas : X RPL 3");
+        System.out.println("No    : 7");
+    }
+}
