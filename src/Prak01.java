8fe4168b9a78b69d4a90f8e659991ea0e317f27c
asus <asus@LAPTOP-1G900J8S>
2018-08-10 08:31:05 +0700

    Initial Commit

---
+++ src/Prak01.java
@@ -0,0 +1,8 @@
+public class Prak01 
+{
+    public static void main(String[] args)
+    {
+        String identitas = "Arya Putra Adyartama / X RPL 3 / 7";
+        System.out.println("Identitas : " + identitas);
+    }
+}
