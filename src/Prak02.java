da08212d996c6afbb4ce681898b100848c8f461d
asus <asus@LAPTOP-1G900J8S>
2018-08-13 17:14:35 +0700

    Initial Commit

---
+++ src/Prak02.java
@@ -0,0 +1,13 @@
+public class Prak02
+{
+	private static void tampilJudul(String identitas)
+	{
+	System.out.println("Identitas" + identitas);
+	System.out.println("\nCovert Kalimat Alay Angka (Vokal Ke Angka)\n");
+	}
+	public static void main(String[] args)
+	{
+		String identitas = " Arya Putra Adyartama / XR3 / 7";
+		tampilJudul(identitas);
+	}
+}
